05ef44decd11746dcba00ec2d13e5eb10eea1dff
Jeff Brock <betapi1739@gmail.com>
2014-03-25 09:16:52 -0700

    Initial Commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+		AssemblyCSharp.SceneManager sceneMgr;
+
+		// Use this for initialization
+		void Start ()
+		{
+				var txtToPrint = "Start called!"; 
+				Debug.Log (txtToPrint);
+				sceneMgr = new AssemblyCSharp.SceneManager ();
+				sceneMgr.currentObject = UnityEngine.GameObject.Find ("tempblock");
+		}
+	
+		// Update is called once per frame
+		int frameCounter = 0;
+		void Update ()
+		{
+				//eh, this depends on the frame through, I will need to switch this to time #TODO
+
+				if (frameCounter == 60) {
+						sceneMgr.Tick ();
+						frameCounter = 0;
+				} else {
+						frameCounter++;
+				}
+				//var txtToPrint="Update called!"; 
+				//Debug.Log(txtToPrint);
+		}
+}
---
+++ Assets/PlayerControl.js
@@ -0,0 +1,44 @@
+﻿#pragma strict
+
+var moveUp : KeyCode;
+var moveDown : KeyCode;
+var moveRight : KeyCode;
+var moveLeft : KeyCode;
+
+//var speed : float - 10;
+var speed = -.5;
+
+var counter = 0;
+function Update ()
+{
+
+	if (Input.GetKey(moveUp))
+	{
+		//rigidbody2D.velocity.y = speed;
+		rigidbody2D.transform.position.y += 1;
+		//Debug.Log("MoveUp pressed");
+	}
+	else if (Input.GetKey(moveDown))
+	{
+		//rigidbody2D.velocity.y = speed *-1;
+		rigidbody2D.transform.position.y -= 1;
+		//Debug.Log("MoveDown pressed");
+	}
+	
+	if (Input.GetKey(moveRight))
+	{
+		//rigidbody2D.velocity.y = speed;
+		if (rigidbody2D.transform.position.x < 10){
+		rigidbody2D.transform.position.x += 1;
+		}
+		//Debug.Log("MoveUp pressed");
+	}
+	else if (Input.GetKey(moveLeft))
+	{
+		//rigidbody2D.velocity.y = speed *-1;
+		if (rigidbody2D.transform.position.x > 0){
+		rigidbody2D.transform.position.x -= 1;
+		}
+		//Debug.Log("MoveDown pressed");
+	}
+}
