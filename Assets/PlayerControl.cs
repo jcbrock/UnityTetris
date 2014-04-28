0d53129a291be6a625e2571d6f30e58d628cdbdf
= <=>
2014-04-28 06:35:27 -0700

    Initial testing of bit array

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -11,51 +11,65 @@ using UnityEngine;
 using System.Collections;
 namespace AssemblyCSharp
 {
-	public class PlayerControl : MonoBehaviour
-	{		
-		void Update ()
-		{
-			UnityEngine.Vector3 movementVector = new UnityEngine.Vector3 (0, 0, 0);
-			if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-				movementVector.x = -1.0f;
-			} else if (Input.GetKeyDown (KeyCode.RightArrow)) { 
-				movementVector.x = 1.0f;
-			} else if (Input.GetKeyDown (KeyCode.DownArrow)) { 
-				movementVector.y = -1.0f;
-			}
-			if (movementVector.x != 0 || movementVector.y != 0) {
-				if (!NewBehaviourScript.sceneMgr.CurrentShape.CheckCollisionWithAnyWall (movementVector) && !NewBehaviourScript.sceneMgr.DoAnyShapesCollideInScene (movementVector))									
-					transform.Translate (movementVector, UnityEngine.Space.World);			
-			}
+		public class PlayerControl : MonoBehaviour
+		{		
+				void Update ()
+				{
+						UnityEngine.Vector3 movementVector = new UnityEngine.Vector3 (0, 0, 0);
+						if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+								movementVector.x = -1.0f;
+						} else if (Input.GetKeyDown (KeyCode.RightArrow)) { 
+								movementVector.x = 1.0f;
+						} else if (Input.GetKeyDown (KeyCode.DownArrow)) { 
+								movementVector.y = -1.0f;
+						}
+						if (movementVector.x != 0 || movementVector.y != 0) {
+								if (!NewBehaviourScript.sceneMgr.CurrentShape.CheckCollisionWithAnyWall (movementVector) && !NewBehaviourScript.sceneMgr.DoAnyShapesCollideInScene (movementVector)) {
+										NewBehaviourScript.sceneMgr.UpdateBitArray (false);
+					
+										transform.Translate (movementVector, UnityEngine.Space.World);	
+					
+										NewBehaviourScript.sceneMgr.UpdateBitArray (true);
+										//	NewBehaviourScript.sceneMgr.PrintBitArray ();
 
-			if (Input.GetKeyDown (KeyCode.UpArrow)) {							
-				NewBehaviourScript.sceneMgr.CurrentShape.Rotate ();
-			}
+												
+								}
+						}
 
-			//Debug functions
-			if (Input.GetKeyDown (KeyCode.Keypad1)) { 
-				UnityEngine.Debug.Log ("Row -25 block count: " + NewBehaviourScript.sceneMgr.GetRowCount (-25));
-			}
-			if (Input.GetKeyDown (KeyCode.Keypad2)) { 
-				UnityEngine.Debug.Log ("Row -24 block count: " + NewBehaviourScript.sceneMgr.GetRowCount (-24));
-			}
-			if (Input.GetKeyDown (KeyCode.Keypad3)) { 
-				UnityEngine.Debug.Log ("Row -23 block count: " + NewBehaviourScript.sceneMgr.GetRowCount (-23));
-			}
-			if (Input.GetKeyDown (KeyCode.Keypad4)) { 
-				UnityEngine.Debug.Log ("Row -22 block count: " + NewBehaviourScript.sceneMgr.GetRowCount (-22));
-			}
-			if (Input.GetKeyDown (KeyCode.Keypad5)) { 
-				UnityEngine.Debug.Log ("Row -21 block count: " + NewBehaviourScript.sceneMgr.GetRowCount (-21));
-			}
-			if (Input.GetKeyDown (KeyCode.Keypad6)) { 
-				UnityEngine.Debug.Log ("Row -20 block count: " + NewBehaviourScript.sceneMgr.GetRowCount (-20));
-			}
-			if (Input.GetKeyDown (KeyCode.Keypad7)) { 
-				UnityEngine.Debug.Log ("Row -19 block count: " + NewBehaviourScript.sceneMgr.GetRowCount (-19));
-			}
+						if (Input.GetKeyDown (KeyCode.UpArrow)) {				
+								NewBehaviourScript.sceneMgr.UpdateBitArray (false);
+				
+								NewBehaviourScript.sceneMgr.CurrentShape.Rotate ();
+				
+								NewBehaviourScript.sceneMgr.UpdateBitArray (true);
+								//NewBehaviourScript.sceneMgr.PrintBitArray ();
+								
+						}
+
+						//Debug functions
+						if (Input.GetKeyDown (KeyCode.Keypad1)) { 
+								UnityEngine.Debug.Log ("Row -25 block count: " + NewBehaviourScript.sceneMgr.GetRowCount (-25));
+						}
+						if (Input.GetKeyDown (KeyCode.Keypad2)) { 
+								UnityEngine.Debug.Log ("Row -24 block count: " + NewBehaviourScript.sceneMgr.GetRowCount (-24));
+						}
+						if (Input.GetKeyDown (KeyCode.Keypad3)) { 
+								UnityEngine.Debug.Log ("Row -23 block count: " + NewBehaviourScript.sceneMgr.GetRowCount (-23));
+						}
+						if (Input.GetKeyDown (KeyCode.Keypad4)) { 
+								UnityEngine.Debug.Log ("Row -22 block count: " + NewBehaviourScript.sceneMgr.GetRowCount (-22));
+						}
+						if (Input.GetKeyDown (KeyCode.Keypad5)) { 
+								UnityEngine.Debug.Log ("Row -21 block count: " + NewBehaviourScript.sceneMgr.GetRowCount (-21));
+						}
+						if (Input.GetKeyDown (KeyCode.Keypad6)) { 
+								UnityEngine.Debug.Log ("Row -20 block count: " + NewBehaviourScript.sceneMgr.GetRowCount (-20));
+						}
+						if (Input.GetKeyDown (KeyCode.Keypad7)) { 
+								UnityEngine.Debug.Log ("Row -19 block count: " + NewBehaviourScript.sceneMgr.GetRowCount (-19));
+						}
+				}
 		}
-	}
 }
 
 
